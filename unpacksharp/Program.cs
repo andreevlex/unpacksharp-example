518d4795b958154715bfe10282a4f5ca7f048300
Alexander Andreev <andreevlex.as@gmail.com>
2017-12-27 21:57:53 +0300

    First build

--- unpacksharp/Program.cs
+++ unpacksharp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace unpacksharp
 {
     class MainClass
     {
+		[DllImport("libv8unpack4rs.so")]
+	    private static extern bool parse_cf(IntPtr pFileName, IntPtr pDirName);
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+	        if (args.Length < 3)
+	        {
+		        usage();
+		        Environment.Exit(-1);
+	        }
+
+	        string command = args[0];
+
+	        if (command == "--parse")
+	        {
+		        string fileName = args[1];
+		        string dirName = args[2];
+		        
+		        IntPtr pFileName = Marshal.StringToHGlobalAnsi(fileName);
+                IntPtr pDirName = Marshal.StringToHGlobalAnsi(dirName);
+                var result_parse = parse_cf(pFileName, pDirName);
+                Console.WriteLine("parse_cf: {0}", result_parse);
+	        }
+	        
+			//Console.ReadLine();
         }
+
+	    private static void usage()
+	    {
+		    Console.WriteLine("unpacksharp --parse file_name dir_name");
+	    }
     }
 }
