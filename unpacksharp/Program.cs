81ebae45275669fc808430be7c04b2d76787a05c
Alexander Andreev <andreevlex.as@gmail.com>
2017-12-27 22:18:26 +0300

    Форматирование

--- unpacksharp/Program.cs
+++ unpacksharp/Program.cs
@@ -4,38 +4,38 @@ using System.Text;
 
 namespace unpacksharp
 {
-    class MainClass
-    {
+	class MainClass
+	{
 		[DllImport("libv8unpack4rs.so")]
-	    private static extern bool parse_cf(IntPtr pFileName, IntPtr pDirName);
+		private static extern bool parse_cf(IntPtr pFileName, IntPtr pDirName);
 
-        public static void Main(string[] args)
-        {
-	        if (args.Length < 3)
-	        {
-		        usage();
-		        Environment.Exit(-1);
-	        }
+		public static void Main(string[] args)
+		{
+			if (args.Length < 3)
+			{
+				Usage();
+				Environment.Exit(-1);
+			}
 
-	        string command = args[0];
+			string command = args[0];
 
-	        if (command == "--parse")
-	        {
-		        string fileName = args[1];
-		        string dirName = args[2];
-		        
-		        IntPtr pFileName = Marshal.StringToHGlobalAnsi(fileName);
-                IntPtr pDirName = Marshal.StringToHGlobalAnsi(dirName);
-                var result_parse = parse_cf(pFileName, pDirName);
-                Console.WriteLine("parse_cf: {0}", result_parse);
-	        }
-	        
+			if (command == "--parse")
+			{
+				string fileName = args[1];
+				string dirName = args[2];
+				
+				IntPtr pFileName = Marshal.StringToHGlobalAnsi(fileName);
+				IntPtr pDirName = Marshal.StringToHGlobalAnsi(dirName);
+				var resultParse = parse_cf(pFileName, pDirName);
+				Console.WriteLine("parse_cf: {0}", resultParse);
+			}
+			
 			//Console.ReadLine();
-        }
+		}
 
-	    private static void usage()
-	    {
-		    Console.WriteLine("unpacksharp --parse file_name dir_name");
-	    }
-    }
+		private static void Usage()
+		{
+			Console.WriteLine("unpacksharp --parse file_name dir_name");
+		}
+	}
 }
