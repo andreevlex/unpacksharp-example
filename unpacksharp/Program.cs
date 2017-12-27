1a3ff517d9d51f4c5d1706cc4388b02b8c94d9ea
Alexander Andreev <andreevlex.as@gmail.com>
2017-12-27 21:54:55 +0300

    Init

---
+++ unpacksharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace unpacksharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
